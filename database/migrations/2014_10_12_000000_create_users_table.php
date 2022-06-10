b5f6ee0b52e8e6071fc2d637d292c84bebefbf12
JamesEditionZz <kanpongpgm@gmail.com>
2022-06-10 15:53:49 +0700

    update (create table and insert template)

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -26,6 +26,7 @@ return new class extends Migration
             $table->timestamps();
             $table->string('pack_id');
             $table->string('status');
+            $table->string('template');
         });
     }
 
---
+++ resources/views/template.blade.php
@@ -0,0 +1,40 @@
+<x-app-layout>
+    <x-slot name="header">
+    
+
+    <div class="py-6">
+        <div class="container">
+            <div class="row">
+              <div class="col-sm-3">
+                <p>เลือกประเภทธุรกิจ</p>
+                <select class="form-select" name="selectbus">
+                    <option></option>
+                </select>
+              </div>
+              <div class="col-sm-3">
+                <p>เลือกประเภทเว็บไซต์</p>
+                <select class="form-select" name="selectweb">
+                    <option></option>
+                </select>
+              </div>
+              <div class="col-sm-3">
+                <p>เลือก Mood&Tone ของสี</p>
+                <select class="form-select" name="selectweb">
+                    <option></option>
+                </select>
+              </div>
+              <div class="col-sm-3">
+                <p>เลือก Mood&Tone ของFont(ตัวอักษร)</p>
+                <select class="form-select" name="selectweb">
+                    <option></option>
+                </select>
+              </div>
+            </div>
+        </div>
+        <br>
+        <div class="container p-2 text-center">
+            <h3>กรุณาเลือกธีม</h3>
+        </div>
+    </div>
+</x-slot>
+</x-app-layout>
